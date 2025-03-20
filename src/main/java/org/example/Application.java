6b77489dded2b312d4f4f74cfc3b9615ad0c6f47
sozin <sozin.vladislav@mail.ru>
2025-03-20 09:04:00 +0300

    250320 0903 commit

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -5,8 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-
 	public static void main(String[] args) {
+//		Сайт, где смотреть результаты: http://localhost:8080/h2-console
+//		При входе на страницу: http://localhost:8080/h2-console
+//		Укажите следующие параметры:
+//		Saved Settings: Generic H2 (Embedded)
+//		Setting Name: Generic H2 (Embedded)
+//		Driver Class: org.h2.Driver
+//		JDBC URL: jdbc:h2:mem:testdb
+//		User Name: sa
+//		Password: (оставьте поле пустым)
+
 		SpringApplication.run(Application.class, args);
 	}
 
